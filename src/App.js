3e52ae1712ec13a9cb5c7204c3f845a7edd3cedf
James Doss <jamesdoss@Jamess-MBP-2.attlocal.net>
2022-10-22 13:00:22 -0500

    fixed friday count down timer

--- src/App.js
+++ src/App.js
@@ -9,7 +9,24 @@ const App = () => {
   const THREE_DAYS_IN_MS = 7*24*60*60*1000;
   const NOW_IN_MS = new Date().getTime();
 
-  const timeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
+  // console.log(new Date().getDate() + ((7 - NOW_IN_MS.getDay() + 5)))
+    const date = new Date();
+    const dateCopy = new Date(date.getTime());
+    const nextFriday = new Date(
+      dateCopy.setDate(
+        dateCopy.getDate() + ((7 - dateCopy.getDay() + 5) % 7 || 7)
+      )
+    );  
+    console.log('next firday', nextFriday.getTime());
+
+    const acutalNextFriday = nextFriday.getTime();
+
+
+  console.log('next friday plus the timer rules', nextFriday - THREE_DAYS_IN_MS)
+  
+
+  const timeAfterThreeDays = acutalNextFriday;
+  
   return (
     <div>
       <Routes>
