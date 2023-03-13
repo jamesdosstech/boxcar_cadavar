2ec61059e128ff1d9862a7c9065a553b57ad5a12
jamesdosstech <jamesdosstech@gmail.com>
2023-03-13 15:17:57 -0500

    new profile component - may change displayName

---
+++ src/components/displayNameInput/display-name-input.component.jsx
@@ -0,0 +1,64 @@
+import { useState, useContext } from "react";
+import { UserContext } from "../../context/user/user.context";
+import FormInput from "../form-input/form-input.component";
+import { auth } from "../../utils/firebase/firebase.utils";
+import { onAuthStateChanged, updateProfile } from "firebase/auth";
+import "./display-name-input.styles.scss";
+import Button from "../button/button.component";
+
+const defaultFormFields = {
+  displayName: "",
+};
+
+const DisplayNameInput = () => {
+  const [drop, setDrop] = useState(false);
+
+  const { currentUser } = useContext(UserContext);
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName } = formFields;
+
+  //firebase logic
+  console.log(auth.currentUser.displayName);
+
+  const toggleChangeName = () => {
+    setDrop(!drop);
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    // e.preventDefault();
+    console.log("submit");
+    await updateProfile(auth.currentUser, {
+      displayName: displayName,
+    });
+  };
+
+  return (
+    <>
+      <div onClick={toggleChangeName}>{currentUser.displayName}</div>
+      {drop ? (
+        <div className="absolute-block">
+          <form onSubmit={handleSubmit}>
+            <FormInput
+              type="text"
+              label="New Display Name"
+              name="displayName"
+              value={displayName}
+              onChange={handleChange}
+              required
+            />
+            <Button>Submit</Button>
+          </form>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
+  );
+};
+
+export default DisplayNameInput;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,51 +1,49 @@
-import { Outlet, Link } from 'react-router-dom';
-import './navigation.styles.scss';
-import { useContext } from 'react';
+import { Outlet, Link } from "react-router-dom";
+import "./navigation.styles.scss";
+import { useContext } from "react";
 
-import { ReactComponent as HomeIcon} from '../../assets/train-icon.svg'
+import { ReactComponent as HomeIcon } from "../../assets/train-icon.svg";
 
-import { UserContext } from '../../context/user/user.context';
-import { signOutUser } from '../../utils/firebase/firebase.utils';
+import { UserContext } from "../../context/user/user.context";
+import { signOutUser } from "../../utils/firebase/firebase.utils";
+import DisplayNameInput from "../../components/displayNameInput/display-name-input.component";
 
 const Navigation = () => {
   const { currentUser } = useContext(UserContext);
   // console.log(currentUser);
   return (
     <>
-      <div className='navigation-container'>
-        <div className='nav-col'>
-          <Link to='/'>
-            <HomeIcon style={{width: '33', height: '20px', paddingTop: '10px', paddingLeft: '10px'}}/>
+      <div className="navigation-container">
+        <div className="nav-col">
+          <Link to="/">
+            <HomeIcon
+              style={{
+                width: "33",
+                height: "20px",
+                paddingTop: "10px",
+                paddingLeft: "10px",
+              }}
+            />
           </Link>
         </div>
-        <div className='nav-col' style={{textDecoration: 'none'}}>
-          <Link to='/shop'>
-            Shop
-          </Link>
+        <div className="nav-col" style={{ textDecoration: "none" }}>
+          <Link to="/shop">Shop</Link>
         </div>
-        <div className='nav-col'>
-          {
-            currentUser ?
-            currentUser.displayName :
-            ''
-          }
-        </div>
-        
-        <div className='nav-col'>
-          {
-            currentUser ? (
-              <div onClick={signOutUser}>Sign Out</div>
-            ) : (
-              <Link to='/sign-in'>
-                <div>Sign In</div>
-              </Link>
-            )
-          }
+        <div className="nav-col">{currentUser ? <DisplayNameInput /> : ""}</div>
+
+        <div className="nav-col">
+          {currentUser ? (
+            <div onClick={signOutUser}>Sign Out</div>
+          ) : (
+            <Link to="/sign-in">
+              <div>Sign In</div>
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
     </>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
