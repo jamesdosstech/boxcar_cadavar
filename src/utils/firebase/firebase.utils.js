80f1d57c39ded4c5656e181e3fbe73078db9282d
James Doss <jamesdosstech@gmail.com>
2023-10-12 12:20:48 -0500

    The Roof...The Roof is on Fire

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -32,14 +32,25 @@ import {
 } from 'firebase/storage'
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyAsxtaefK62L21AezkX6S4sDCRj90lU7DQ",
+    authDomain: "doosetrain-52f13.firebaseapp.com",
+    projectId: "doosetrain-52f13",
+    storageBucket: "doosetrain-52f13.appspot.com",
+    messagingSenderId: "279844528484",
+    appId: "1:279844528484:web:4d55f1956b7de676c49a71",
+    measurementId: "G-DSHDM3JS4Q"
 };
+
+
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+// };
 
 // eslint-disable-next-line
 // const firebaseApp = initializeApp(firebaseConfig)
