968b421d141c1fc7734e84d6092e6a93746678e0
James Doss <jamesdosstech@gmail.com>
2023-10-13 01:14:08 -0500

    revert shop back to construction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 
 import Splash from "./routes/splash/splash.component";
 import Showroom from "./routes/showroom/showroom.component";
@@ -15,7 +15,9 @@ import DBNewItems from "./components/DBSections/DBNewItems";
 import DBUsers from "./components/DBSections/DBUsers";
 import DoosetrainStore from "./routes/store/DoosetrainStore";
 import Checkout from "./routes/checkout/checkout.component";
-
+import { useContext } from "react";
+import { UserContext } from "./context/user/user.context";
+import ProductEdit from './routes/ProductEdit/ProductEdit'
 const trainList = [
   {
     id: 0,
@@ -66,6 +68,8 @@ const App = () => {
 
   const dayAndHourOfShow = nextFriday.getTime();
 
+  const { currentUser } = useContext(UserContext)
+
   return (
     <div>
       <Routes>
@@ -83,19 +87,12 @@ const App = () => {
           <Route path="/showroom" element={<Showroom />} />
           <Route path="/pass-reset" element={<ResetPassword />} />
           <Route path="sign-in" element={<Authentication />} />
-          <Route path='shop' element={<DoosetrainStore />} />
-          <Route path='admin/*' element={<Dashboard />} >
-            <Route path='Home' element={<DBHome />} />
-            <Route path='Orders' element={<DBOrders />} />
-            <Route path='Products' element={<DBItems />} />
-            <Route path='NewProducts' element={<DBNewItems />} />
-            <Route path='Users' element={<DBUsers />} />
-          </Route>
+          <Route path='shop' element={<UnderConstruction />} />
+          {/* <Route path='admin/*' element={currentUser && currentUser.email === process.env.REACT_APP_ADMIN_EMAIL ? <Dashboard /> : <Navigate to='/sign-in' />} /> */}
           <Route path="checkout" element={<Checkout />} />
-          {/* this may not work */}
         </Route>
       </Routes>
-    </div>
+    </div >
   );
 };
 
