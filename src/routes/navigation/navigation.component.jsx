d7fae49e7b076bb8e7629a493095de1da9775dd1
jamesdosstech <jamesdosstech@gmail.com>
2024-02-28 14:23:46 -0600

    navigation pane reflects session storage display name

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -7,33 +7,34 @@ import { ReactComponent as HomeIcon } from "../../assets/train-icon.svg";
 import { UserContext } from "../../context/user/user.context";
 import { auth, signOutUser } from "../../utils/firebase/firebase.utils";
 import { updateProfile } from "firebase/auth";
+import { DisplayNameContext } from "../../context/displayName/DisplayName.context";
 
-const defaultFormFields = {
-  displayName: "",
-};
+// const defaultFormFields = {
+//   displayName: "",
+// };
 
 const Navigation = () => {
-  const adminEmail = process.env.REACT_APP_ADMIN_EMAIL
-  const { currentUser } = useContext(UserContext);
+  const adminEmail = process.env.REACT_APP_ADMIN_EMAIL;
+  const { displayName } = useContext(DisplayNameContext);
 
-  const [formFields, setFormFields] = useState(defaultFormFields);
-  const { displayName } = formFields;
+  // const [formFields, setFormFields] = useState(defaultFormFields);
+  // const { displayName } = formFields;
 
   //firebase logic
   // console.log(auth);
 
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormFields({ ...formFields, [name]: value });
-  };
+  // const handleChange = (event) => {
+  //   const { name, value } = event.target;
+  //   setFormFields({ ...formFields, [name]: value });
+  // };
 
-  const handleSubmit = async (e) => {
-    // e.preventDefault();
-    console.log("submit");
-    await updateProfile(auth.currentUser, {
-      displayName: displayName,
-    });
-  };
+  // const handleSubmit = async (e) => {
+  //   // e.preventDefault();
+  //   console.log("submit");
+  //   await updateProfile(auth.currentUser, {
+  //     displayName: displayName,
+  //   });
+  // };
 
   return (
     <>
@@ -44,61 +45,92 @@ const Navigation = () => {
               style={{
                 width: "30%",
                 height: "30%",
-                paddingRight: '5px'
+                paddingRight: "5px",
                 // paddingTop: "10px",
                 // paddingLeft: "10px",
               }}
             />
             Doosetrain
           </Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarNavAltMarkup"
+          >
             <div className="navbar-nav">
               <div>
-                <Link className="nav-item nav-link" to="/shop">Shop</Link>
+                <Link className="nav-item nav-link" to="/shop">
+                  Shop
+                </Link>
               </div>
               <div>
-                <Link className="nav-item nav-link" to="/showroom">Showroom</Link>
+                <Link className="nav-item nav-link" to="/showroom">
+                  Showroom
+                </Link>
               </div>
-              <div>
+              {/* <div>
                 {currentUser && currentUser.email === adminEmail && <Link className="nav-item nav-link" to="/admin">Admin</Link>}
+              </div> */}
+              {/* {currentUser && ( */}
+              <div className="nav-item dropdown">
+                <a
+                  className="nav-link "
+                  // href="#"
+                  // id="navbarDropdownMenuLink"
+                  // data-toggle="dropdown"
+                  // aria-haspopup="true"
+                  // aria-expanded="false"
+                >
+                  {displayName && displayName}
+                </a>
+                {/* <div
+                  className="dropdown-menu"
+                  aria-labelledby="navbarDropdownMenuLink"
+                >
+                  <form className="form-inline" onSubmit={handleSubmit}>
+                    <input
+                      className="form-control mr-sm-2"
+                      type="search"
+                      name="displayName"
+                      value={displayName}
+                      onChange={handleChange}
+                      required
+                      maxLength={15}
+                      placeholder={"change your display"}
+                      aria-label="Change Username"
+                    />
+                    <button
+                      className="btn btn-outline-success my-2 my-sm-0"
+                      type="submit"
+                    >
+                      Submit
+                    </button>
+                  </form>
+                </div> */}
               </div>
-              { currentUser && (
-                  <div className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      {currentUser && currentUser.displayName}
-                    </a>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                      <form className="form-inline" onSubmit={handleSubmit}>
-                        <input 
-                          className="form-control mr-sm-2" 
-                          type="search" 
-                          name="displayName"
-                          value={displayName}
-                          onChange={handleChange}
-                          required
-                          maxLength={15}
-                          placeholder={'change your display'}
-                          aria-label="Change Username" 
-                        />
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Submit</button>
-                      </form>
-                    </div>
-                  </div>  
-                )
-              }
-              
-              <div>
+              {/* )} */}
+
+              {/* <div>
                 {currentUser ? (
-                  <Link className="nav-item nav-link" onClick={signOutUser}>Sign Out</Link>
+                  <Link className="nav-item nav-link" onClick={signOutUser}>
+                    Sign Out
+                  </Link>
                 ) : (
                   <Link className="nav-item nav-link" to="/sign-in">
                     <div>Sign In</div>
                   </Link>
                 )}
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
