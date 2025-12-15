f7dffb434803220505d031301728ffa195697f99
James Doss <jamesdosstech@gmail.com>
2025-12-15 14:09:44 -0600

    Fixed Timer added new Loader

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import svgr from '@svgr/rollup'
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [svgr(), react()],
     esbuild: {
         loader: 'jsx',
         include: /src\/.*\.js$/,  // parse JSX in .js files
